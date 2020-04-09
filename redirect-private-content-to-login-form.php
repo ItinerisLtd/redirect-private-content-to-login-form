61e4006dc8120cf3e51de8d3bf06b5dad387017a
TangRufus <tangrufus@gmail.com>
2020-04-09 11:19:18 +0100

    :four::zero::one: 401

---
+++ redirect-private-content-to-login-form.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Plugin Name:         Redirect private page to login form
+ * Plugin URI:          https://github.com/itinerisltd/redirect-private-content-to-login-form
+ * Description:         Redirect private content to login form
+ * Version:             0.1.0
+ * Requires at least:   5.0
+ * Requires PHP:        7.2.0
+ * Author:              Itineris Limited
+ * Author URI:          https://www.itineris.co.uk/
+ * License:             MIT
+ * Text Domain:         redirect-private-content-to-login-form
+ */
+
+declare(strict_types=1);
+
+namespace Itineris\RedirectPrivateContentToLoginForm;
+
+add_action('template_redirect', function (): void {
+    global $wp_query, $wpdb;
+
+    if (! is_404()) {
+        return;
+    }
+
+    $private = $wpdb->get_row($wp_query->request);
+    if (! is_object($private)) {
+        return;
+    }
+
+    $postStatus = $private->post_status ?? null;
+    if ('private' !== $postStatus) {
+        return;
+    }
+
+    $currentUrl = home_url(
+        add_query_arg(null, null)
+    );
+    $loginUrl = wp_login_url($currentUrl);
+
+    wp_safe_redirect($loginUrl);
+    exit;
+}, 9);
